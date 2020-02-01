a04fd5e91a91f03649d0c8d8fabaf9bfb99f8042
Xime-Tapia <59945827+Xime-Tapia@users.noreply.github.com>
2020-01-31 23:46:33 -0500

    EJERCICIO DE MODULSO

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 12:08:33 2020
+
+@author: CEC
+"""
+
+import modulo.py
---
+++ modulo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 12:08:33 2020
+
+@author: CEC
+"""
+
+# print ("QUIERO SER UN MODULO")
+
+if __name__ == "__main__" :
+    print ("I prefer to be a module")
+else:
+    print ("I like to be a module")
